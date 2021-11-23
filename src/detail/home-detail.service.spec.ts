92a6f727b92da502a717c04aac1e0bfeb5073053
salma-venturas <salma.kona@venturas-bd.com>
2021-11-23 13:40:31 +0600

    unit test added for service

--- src/detail/home-detail.service.spec.ts
+++ src/detail/home-detail.service.spec.ts
@@ -5,16 +5,58 @@ import { HomeDetailService } from "./home-detail.service";
 
 describe("HomeDetailService", () => {
   let service: HomeDetailService;
- 
+  const mockHomeDetailRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    findOne: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         HomeDetailService,
-       
+        {
+          provide: getRepositoryToken(HomeDetail),
+          useValue: mockHomeDetailRepository,
+        },
       ],
     }).compile();
 
     service = module.get<HomeDetailService>(HomeDetailService);
+  });
+
+  it("should be create a new data and return", async () => {
+    mockHomeDetailRepository.create.mockReturnValue({
+      title: "qui est esse",
+      body: "est rerum tempore",
+    }),
+      mockHomeDetailRepository.save.mockReturnValue({
+        id: 1,
+        title: "qui est esse",
+        body: "est rerum tempore",
+      });
+    const data: HomeDetail = await service.create({
+      title: "qui est esse",
+      body: "est rerum tempore",
+    });
+    expect(data).toStrictEqual({
+      id: 1,
+      title: "qui est esse",
+      body: "est rerum tempore",
+    });
+  });
+
+  it("should be retrive one data by id", async () => {
+    mockHomeDetailRepository.findOne.mockReturnValue({
+      id: 2,
+      title: "This is test title",
+      body: "This is test body",
+    });
+    const homes = mockHomeDetailRepository.findOne();
+    expect(homes).toEqual({
+      id: 2,
+      title: "This is test title",
+      body: "This is test body",
+    });
   });
 });
