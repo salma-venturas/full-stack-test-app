dea208196211f00e60d59d7757f3d707f79a9b2a
salma-venturas <salma.kona@venturas-bd.com>
2021-11-23 15:40:29 +0600

    removed unused code

--- Angular/src/app/home-detail/home-detail.component.ts
+++ Angular/src/app/home-detail/home-detail.component.ts
@@ -23,20 +23,6 @@ export class HomeDetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // let id = this.route.snapshot.paramMap.get('id');
-    // this.homeDetailService.getDeatil(id).subscribe({
-    //   next: (respose: detail) => {
-    //     if (!respose.title || !respose.body) {
-    //       this.router.navigate(['/404']);
-    //     } else {
-    //       this.detail = respose;
-    //     }
-    //   },
-    //   error: (e) => {
-    //     this.router.navigate(['/404']);
-    //   },
-    //   complete: () => console.info('complete'),
-    // });
     this.commonService.getData().subscribe({
       next:(value)=>{this.detail=value}
     })
