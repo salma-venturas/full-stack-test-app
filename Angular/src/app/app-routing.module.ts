b21aee5492c95655bd635eb7955ec79a4135f19d
salma-venturas <salma.kona@venturas-bd.com>
2021-11-23 13:03:27 +0600

    init angular app

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeEntryComponent } from './home-entry/home-entry.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+
 
 
 const routes: Routes = [
-  { path: '', component: HomeEntryComponent },
-  {
-    path: 'detail',
-    loadChildren: () => import('./home-detail/home-detail.module').then(m => m.HomeDetailModule)
-  },
-  {path: '404', component: NotFoundComponent},
+
 ];
 
 @NgModule({
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeEntryComponent } from './home-entry/home-entry.component';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { NotFoundComponent } from './not-found/not-found.component';
+
 
 @NgModule({
-  declarations: [AppComponent, HomeEntryComponent, NotFoundComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule,ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
